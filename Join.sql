fc7aadec737cea3ef110d80f42072e49416f8f42
Sadikshya077 <asadikshya07@gmail.com>
2026-09-16 07:49:32 +0545

    left, right and outer join

--- Join.sql
+++ Join.sql
@@ -63,3 +63,49 @@ from sales.staffs s1
 join sales.staffs s2
 on s1.staff_id = s2.manager_id;
 
+-- cross join -> cross join is performed in the background of all joins
+-- customer_id 1 searches for all customer_id 1 in order table and gives output, similarly for all.
+-- cartesian product
+select * from sales.customers
+cross join sales.orders --takes time to execute
+
+-- we use filter
+select * from sales.customers sc
+cross join sales.orders so
+where (sc.customer_id=1 and so.customer_id=1)
+	or (sc.customer_id=2 and so.customer_id=2) --does for all id when we run without using filter
+order by 2 desc; -- second column is ordered in descending order
+
+-- Find total staffs, total orders and total customers managed by managers
+
+select 
+concat(s1.first_name,' ',s1.last_name) as manager_name,
+count(distinct s2.staff_id) as total_staffs,
+count(distinct so.order_id) as total_orders,
+count(distinct sc.customer_id) as total_customers
+from sales.staffs s1
+join sales.staffs s2
+on s1.staff_id = s2.manager_id
+join sales.orders so
+on s1.staff_id = so.staff_id
+join sales.customers sc
+on sc.customer_id = so.customer_id
+group by concat (s1.first_name,' ',s1.last_name);
+
+-- left join
+select * 
+from sales.staffs s1
+left join sales.staffs s2
+on s1.staff_id = s2.manager_id
+
+-- right join
+select *
+from sales.staffs s1
+right join sales.staffs s2
+on s1.staff_id = s2.staff_id
+
+-- outer join
+select *
+from sales.staffs s1
+full outer join sales.staffs s2
+on s1.staff_id = s2.manager_id
