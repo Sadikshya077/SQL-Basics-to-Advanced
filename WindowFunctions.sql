843a7bc8896035c5719cd4fa6879994a5745be4e
Sadikshya077 <asadikshya07@gmail.com>
2026-09-22 22:50:43 +0545

    window functions syntax

---
+++ WindowFunctions.sql
@@ -0,0 +1,15 @@
+/*
+	Window Functions
+	----------------------
+	1. Row_Number() -> To give each row unique identifying value, Find and remove duplicate data
+	2. Rank() -> Ranking data -> skips row number
+	3. Dense_Rank() -> Ranking data -> Doesn't skip row number. Used to find nth highest data.
+	
+	4. NTile(num) -> Divides data
+	5. Lead(col_name, offset(optional)) -> Next Value
+	6. Lag(col_name, offset(optional)) -> Previous Value
+		offset -> how many rows to skip is 1 in default
+	7. Running Sum -> Sum(col_name)
+	8. Moving Average -> Avg(col_name)
+*/
+
