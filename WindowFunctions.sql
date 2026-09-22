6689d4409a33b542490f9cc67691fcc12678fd71
Sadikshya077 <asadikshya07@gmail.com>
2026-09-23 00:05:23 +0545

    Rank and DenseRank

--- WindowFunctions.sql
+++ WindowFunctions.sql
@@ -23,4 +23,59 @@
 */
 
 
+-- Row Number
+select 
+	Transaction_ID, USER_ID, Transaction_Amount, Transaction_Type, Time_of_Transaction,
+	Device_Used, Location, Previous_Fraudulent_Transactions, Account_Age,
+	Number_of_Transactions_Last_24H, Payment_Method, Fraudulent,
+	ROW_NUMBER() OVER(partition by Time_of_Transaction order by Transaction_Amount desc) as row_num--  by default desc
+from Fraud.dbo.[Fraud Detection Dataset]; --if we don't want to change the database manually
+
+-- we can do this simply as this but here the full data is not shown so we use window function
+select Time_of_Transaction, COUNT(Transaction_ID)
+from Fraud.dbo.[Fraud Detection Dataset]
+group by Time_of_Transaction
+
+-- finding duplicates by row number
+select 
+	Transaction_ID, USER_ID, Transaction_Amount, Transaction_Type, Time_of_Transaction,
+	Device_Used, Location, Previous_Fraudulent_Transactions, Account_Age,
+	Number_of_Transactions_Last_24H, Payment_Method, Fraudulent,
+	ROW_NUMBER() OVER(partition by Transaction_ID order by Transaction_ID desc) as row_num--  by default desc
+from Fraud.dbo.[Fraud Detection Dataset]; 
+
+-- here after each unique value in a row, the row number resets so every unique rows will have 1
+-- but in case of duplicate transaction_id there will be two row number 1 and 2
+-- in this way we find the duplicates
+
+
+-- now we use CTE to remove the duplicate values
+-- remove all data whose row_num is more than 1
+with fraud_duplicate_data as (
+	select 
+	Transaction_ID, USER_ID, Transaction_Amount, Transaction_Type, Time_of_Transaction,
+	Device_Used, Location, Previous_Fraudulent_Transactions, Account_Age,
+	Number_of_Transactions_Last_24H, Payment_Method, Fraudulent,
+	ROW_NUMBER() OVER(partition by Transaction_ID order by Transaction_ID desc) as row_num--  by default desc
+from Fraud.dbo.[Fraud Detection Dataset]
+)
+delete from fraud_duplicate_data where row_num >1;
+-- it permanently removes the data on the basis of transaction id
+
+-- Rank
+select product_id, product_name, brand_id, category_id, model_year, list_price,
+RANK() OVER(partition by brand_id order by list_price) as rank_num -- skips the value after ranking
+from BikeStores.production.products
+
+-- Dense Rank
+select product_id, product_name, brand_id, category_id, model_year, list_price,
+DENSE_RANK() OVER(partition by brand_id order by list_price) as dense_rank_num -- doesn't skip value
+from BikeStores.production.products
+
+select * from (
+	select product_id, product_name, brand_id, category_id, model_year, list_price,
+	DENSE_RANK() OVER(partition by model_year order by list_price) as dense_rank_num -- doesn't skip value
+	from BikeStores.production.products
+) as data
+where dense_rank_num = 2 --gives second highest number
 
